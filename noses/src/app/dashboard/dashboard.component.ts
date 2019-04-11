501080c127634e02cd3332a2373a4bd95bf1cb60
Raquel Bonilla <raquelb.2014@gmail.com>
2019-04-10 23:12:16 -0700

    pulling from the rest api into a table

--- noses/src/app/dashboard/dashboard.component.ts
+++ noses/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../backend.service';
 import { Policy } from  '../policy';
+import { MatTableModule, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,13 +12,41 @@ export class DashboardComponent implements OnInit {
 
   policies:  Policy[];
   selectedPolicy:  Policy  = { id :  null , number:null, amount:  null};
+  dataSource: Policy[]
 
   constructor(private apiService: BackendService) { }
 
   ngOnInit() {
     this.apiService.readPolicies().subscribe((policies: Policy[])=>{
       this.policies = policies;
+      this.dataSource = policies;
       console.log(this.policies);
     })
+  }
+
+  createOrUpdatePolicy(form){
+    if(this.selectedPolicy && this.selectedPolicy.id){
+      form.value.id = this.selectedPolicy.id;
+      this.apiService.updatePolicy(form.value).subscribe((policy: Policy)=>{
+        console.log("Policy updated" , policy);
+      });
+    }
+    else{
+
+      this.apiService.createPolicy(form.value).subscribe((policy: Policy)=>{
+        console.log("Policy created, ", policy);
+      });
+    }
+
+  }
+
+  selectPolicy(policy: Policy){
+    this.selectedPolicy = policy;
+  }
+
+  deletePolicy(id){
+    this.apiService.deletePolicy(id).subscribe((policy: Policy)=>{
+      console.log("Policy deleted, ", policy);
+    });
   }
 }
