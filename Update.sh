6950900270d86f0179fc8c5663df8dac042bb2d8
root <root@ip-10-0-0-78.us-west-2.compute.internal>
2019-05-14 05:53:19 +0000

    initial commit of autodeploy script

---
+++ Update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+eval cd /etc/SealQL
+
+gitResults=`git pull`
+
+if [[ $? != 0 ]] ; then
+   error=$?
+   message="System attempted git pull failed with error: $error"
+   echo $message | mail -s "Code pull failed" joseph.buelow@yahoo.com
+   exit 1
+fi
+
+if [[ $gitResults != "Already up to date." ]] ; then
+   eval cd /etc/SealQL/sealqlteam6/noses
+   eval set -o pipefail
+   buildRes=$(eval ng buil)
+   error=${PIPSTATUS[0]}
+   if [[ $error != 0 ]] ; then
+      message="System attempted ng run that failed with error: $error"
+      echo $message | mail -s "Auto-Build Failed" joseph.buelow@yahoo.com
+      #echo $message | mail -s "Auto-Build Failed" raquelb.2014@gmail.com
+      exit 1
+   fi
+fi
+exit 0
