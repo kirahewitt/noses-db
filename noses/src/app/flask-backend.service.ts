0bd1b1d0f86d3d3c70697d8a02da145ba82889cf
Raquel Bonilla <raquelb.2014@gmail.com>
2019-06-09 21:01:59 -0700

    wrong host...

--- noses/src/app/flask-backend.service.ts
+++ noses/src/app/flask-backend.service.ts
@@ -14,7 +14,7 @@ import { SealDataService } from "./seal-data.service";
 export class FlaskBackendService {
   constructor(private httpClient: HttpClient,
               private sealData: SealDataService) {}
-  FLASK_API_SERVER = "http://34.217.54.156/:5000";
+  FLASK_API_SERVER = "http://34.217.54.156:5000";
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
