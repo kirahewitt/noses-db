d4e109f21c6f35afe7cf619f0b7fc953d93f4835
Ian O'Rourke <iworourke@gmail.com>
2019-11-14 13:02:44 -0800

    citizen scientist bulk upload
    citizen scientist bulk upload

--- noses/src/app/citizen-scientist-upload-management/citizen-sci-bulk-upload-main-page/citizen-sci-bulk-upload-main-page.component.ts
+++ noses/src/app/citizen-scientist-upload-management/citizen-sci-bulk-upload-main-page/citizen-sci-bulk-upload-main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'app-citizen-sci-bulk-upload-main-page',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitizenSciBulkUploadMainPageComponent implements OnInit {
 
-  constructor() { }
+  private fileName: string = "No File Selected"
+  private fileData: any[];
+
+
+  constructor(private papa: Papa) { }
 
   ngOnInit() {
+  }
+
+
+  /**
+   * 
+   * @param event 
+   */
+  public handleFileSelect(event) {
+    var files = event.target.files; // FileList object
+    var file = files[0];
+    this.fileName = file.name;
+
+    console.log(file);
+
+    var reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = (event: any) => {
+      var csv = event.target.result; // Content of CSV file
+      
+      this.papa.parse(csv, {
+        skipEmptyLines: true,
+        header: true,
+        complete: (results) => {
+          this.fileData = [results.data, {"isApproved" : 0}];
+        }
+      });
+    }
   }
 
 }
