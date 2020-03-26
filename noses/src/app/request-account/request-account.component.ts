cebff702cab51cdf947747860f7700e769f44576
Ian O'Rourke <iworourke@gmail.com>
2020-03-25 23:38:15 -0700

    improved form layout, form validation
    Improved form layout
    
    Got password validation working passwordConfirmation fields (the ones where they ask you to type the password twice)

--- noses/src/app/request-account/request-account.component.ts
+++ noses/src/app/request-account/request-account.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { sqlUser_full, user_forCreateNewUser } from '../_supporting_classes/sqlUser';
 import { FlaskBackendService } from '../_services/flask-backend.service';
+import { ErrorStateMatcher } from '@angular/material';
+
+
+/**
+ * All this class does is provide us access to the isErrorState method via the ErrorStateMatcher
+ * interface. We're going to create an instance of this object and ultimately attach it to
+ * a form control for which we want to display multi-control-dependent error messages.
+ */
+export class PasswordErrorStateMatcher implements ErrorStateMatcher {
+
+  /**
+   * (A FormControl is a single input field. Think of one input in a mat-form-field)
+   * This function allows us to specify that an error state exists for
+   * a specific form control, when EITHER:
+   *   (1) We have an error at the FormControl level
+   *   (2) **We have an error at the FormGroup
+   * @param control : the form control for which we may want to display an error state if
+   *  we discover there is something wrong at the form level.
+   * @param form : a reference to the form which may have an error state.
+   */
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm) : boolean {
+    return (control.invalid || form.invalid);
+  }
+}
 
 
 /**
  * This class manages two objects
  *   (1) for representing the html template's form
  *   (2) for representing the current state of the data in the form
- * 
  */
 @Component({
   selector: 'app-request-account',
@@ -17,10 +40,10 @@ import { FlaskBackendService } from '../_services/flask-backend.service';
 })
 export class RequestAccountComponent implements OnInit {
 
-
   public form : FormGroup; // need an object to hold the form object
   public newUser : user_forCreateNewUser; // need an object to fill the form
-  
+  public passwordErrorStateMatcher : PasswordErrorStateMatcher; 
+
 
   /**
    * Initializes the new user object to a blank user.
@@ -28,6 +51,7 @@ export class RequestAccountComponent implements OnInit {
    */
   constructor(private formBuilder : FormBuilder, private apiService: FlaskBackendService ) { 
     this.newUser = new user_forCreateNewUser();
+    this.passwordErrorStateMatcher = new PasswordErrorStateMatcher();
   }
 
 
@@ -37,13 +61,28 @@ export class RequestAccountComponent implements OnInit {
    */
   public ngOnInit() {
     this.form = this.formBuilder.group({
-      //                 [<initial value>,              <validator methods>]
       firstName        : [this.newUser.firstName,       Validators.required],
       lastName         : [this.newUser.lastName,        Validators.required],
       email            : [this.newUser.email,           Validators.required],
       password         : [this.newUser.password,        Validators.required],
       passwordConfirm  : [this.newUser.passwordConfirm, Validators.required]
-    });
+    }, 
+    // validators that act upon multiple fields for their condition go here
+    {validator: this.passwordsMatchValidator});
+  }
+
+
+  /**
+   * This validator method determines whether the two form fields 'password' and 'passwordConfirm' match.
+   * If the two fields match, it returns null.
+   * If they do not match, it returns an object consisting of an error name, and a boolean true value,
+   * to indicate that the form has violated the requirement of this validity check.
+   * @param g 
+   */
+  public passwordsMatchValidator(g: FormGroup) {
+    let p = g.get('password').value;
+    let pc = g.get('passwordConfirm').value;
+    return p === pc ? null : {passwordPairMismatch : true};
   }
 
 
--- noses/src/app/reset-password/reset-password.component.ts
+++ noses/src/app/reset-password/reset-password.component.ts
@@ -1,7 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm} from '@angular/forms';
 import { FlaskBackendService } from '../_services/flask-backend.service';
 import { ResetPasswordFormObject } from '../_supporting_classes/ResetPasswordFormObject';
+import { ErrorStateMatcher } from '@angular/material';
+
+
+/**
+ * All this class does is provide us access to the isErrorState method via the ErrorStateMatcher
+ * interface. We're going to create an instance of this object and ultimately attach it to
+ * a form control for which we want to display multi-control-dependent error messages.
+ */
+export class PasswordErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl, form: FormGroupDirective | NgForm): boolean
+  {
+    return (control.invalid || form.invalid);
+  }
+}
+
+
 
 /**
  * 
@@ -14,6 +30,7 @@ import { ResetPasswordFormObject } from '../_supporting_classes/ResetPasswordFor
 export class ResetPasswordComponent implements OnInit {
   public form : FormGroup;
   public resetPassFormObj : ResetPasswordFormObject; 
+  public passwordErrorStateMatcher : PasswordErrorStateMatcher; 
 
 
   /**
@@ -22,6 +39,7 @@ export class ResetPasswordComponent implements OnInit {
    */
   constructor(private formBuilder : FormBuilder, private apiService: FlaskBackendService) { 
     this.resetPassFormObj = new ResetPasswordFormObject();
+    this.passwordErrorStateMatcher = new PasswordErrorStateMatcher();
   }
 
 
@@ -34,7 +52,22 @@ export class ResetPasswordComponent implements OnInit {
       oldPassword: [this.resetPassFormObj.oldPassword, Validators.required],
       newPassword: [this.resetPassFormObj.newPassword, Validators.required],
       newPasswordConfirm: [this.resetPassFormObj.newPasswordConfirm, Validators.required]
-    });
+    },
+    {validator: this.passwordsMatchValidator});
+  }
+
+
+  /**
+   * This validator method determines whether the two form fields 'newPassword' and 'newPasswordConfirm' match.
+   * If the two fields match, it returns null.
+   * If they do not match, it returns an object consisting of an error name, and a boolean true value,
+   * to indicate that the form has violated the requirement of this validity check.
+   * @param g 
+   */
+  public passwordsMatchValidator(g: FormGroup) {
+    let p = g.get('newPassword').value;
+    let pc = g.get('newPasswordConfirm').value;
+    return p === pc ? null : {passwordPairMismatch : true};
   }
 
 
