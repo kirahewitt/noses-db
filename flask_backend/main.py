925dcdd101b3c93cdd36a453e9f70f34ac651091
jlfisher <jfishe13@calpoly.edu>
2021-02-23 15:12:48 -0800

    Added delete functionality for observations

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -96,38 +96,63 @@ def delete_user():
         if request.method == 'POST':
             print("in delete")
             _json = request.json
-            print("obs")
             obs = _json['obsID']
             print(obs)
-            print("tag")
-            tags = _json['tags']
-            print("mark")
-            marks = _json['marks']
 
             print("1")
-            cursor.execute("DELETE FROM ObserveSeal WHERE ObservationID='" + str(obs) + "'")
+            cursor.execute("SELECT * FROM ObserveSeal WHERE ObservationID=" + str(obs))
+            row = cursor.fetchone()
+            sealID = row['SealID']
             print("2")
-            cursor.execute("DELETE FROM ObserveMarks WHERE ObservationID='" + str(obs) + "'")
-            print("3")
-            cursor.execute("DELETE FROM ObserveTags WHERE ObservationID='" + str(obs) + "'") 
+            cursor.execute("SELECT * FROM ObserveMarks WHERE ObservationID=" + str(obs))
+            row = cursor.fetchone()
+            if row is not None:
+              markID = row['MarkID']
+            else :
+              markID = None
 
-            # for t in tags:
-            #     print("4")
-            #     if t is not None:
-            #       cursor.execute("DELETE FROM Tags WHERE TagNumber='" + str(t) + "'")
 
-            # for m in  marks:
-            #     print("5")
-            #     if m is not None:
-            #       cursor.execute("DELETE FROM Marks WHERE Mark='" + str(m) + "'")
-
-            print("6")
-            cursor.execute("DELETE FROM Seals WHERE ObservationID=" + str(obs))
-            print("7")
+            print("4")
             cursor.execute("DELETE FROM Measurements WHERE ObservationID=" + str(obs))
-            print("8")
-            cursor.execute("DELETE FROM Observations WHERE ObservationID=" + str(obs))
+            print("5")
+            cursor.execute("DELETE FROM FieldLeaderForObs WHERE ObservationID=" + str(obs))
+            print("6")
+            cursor.execute("DELETE FROM UploadContainingObs WHERE ObservationID=" + str(obs))
 
+            print("7")
+            cursor.execute("DELETE FROM ObserveSeal WHERE ObservationID=" + str(obs))
+            print("8")
+            cursor.execute("DELETE FROM ObserveMarks WHERE ObservationID=" + str(obs))
+            print("9")
+            cursor.execute("DELETE FROM ObserveTags WHERE ObservationID=" + str(obs))
+
+            print("10")
+            cursor.execute("SELECT * FROM ObserveSeal WHERE SealID=" + str(sealID))
+            row = cursor.fetchone()
+
+            if row is None:
+              print("11")
+              cursor.execute("DELETE FROM Seals WHERE SealID=" + str(sealID))
+            else:
+              nextObservation = row['ObservationID']
+              print("12 " + str(nextObservation))
+              cursor.execute("UPDATE Seals s SET s.ObservationID=" + str(nextObservation) + " WHERE s.SealID=" + str(sealID))
+
+            if markID is not None:
+              print("13")
+              cursor.execute("SELECT * FROM ObserveMarks WHERE MarkID=" + str(markID))
+              row = cursor.fetchone()
+
+              if row is None:
+                print("14")
+                cursor.execute("DELETE FROM Marks WHERE MarkID=" + str(markID))
+              else:
+                nextObservation = row['ObservationID']
+                print("15")
+                cursor.execute("UPDATE Marks m SET m.ObservationID=" + str(nextObservation) + " WHERE m.MarkID=" + str(markID))
+            
+            print("3")
+            cursor.execute("DELETE FROM Observations WHERE ObservationID=" + str(obs))
             conn.commit()
             resp = jsonify('Observation deleted successfully!')
             resp.status_code = 200
@@ -824,7 +849,7 @@ def addNewUser_forAdmin():
       print("Result of getAllUsers - Flask API")
       print(rows)
 
-      sendEmailMessage_newAccountCreatedForUser(email, firstName, lastName, password)
+      # sendEmailMessage_newAccountCreatedForUser(email, firstName, lastName, password)
 
       return resp
     else:
